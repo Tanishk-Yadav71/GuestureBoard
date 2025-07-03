1f964151065ea604cc94f893fbbbbd2c3408b013
Tanishk-Yadav71 <tanishkyadav314@gmail.com>
2025-07-03 22:09:30 +0530

    Update gestureboard.py
    added gradient and color select

--- gestureboard.py
+++ gestureboard.py
@@ -12,9 +12,18 @@ mp_draw = mp.solutions.drawing_utils
 
 # Create a blank canvas
 canvas = np.zeros((480, 640, 3), dtype=np.uint8)
+hsv_gradient = np.zeros((50, 640, 3), dtype=np.uint8)
+for x in range(640):
+    hue = int((x / 640) * 179)
+    hsv_gradient[:, x] = (hue, 255, 255)  # H, S, V
+
+# Convert HSV to BGR for display
+gradient_bar = cv2.cvtColor(hsv_gradient, cv2.COLOR_HSV2BGR)
 
 # To store previous fingertip position
 prev_x, prev_y = 0, 0
+
+
 
 def fingers_up(handLms):
     fingers = []
@@ -39,8 +48,11 @@ while True:
     img = cv2.flip(img, 1)  # Flip for mirror effect
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = hands.process(img_rgb)
-
+    brush_color=(255,255,0)
     curr_x, curr_y = 0, 0
+    img[0:50,0:640]=gradient_bar
+    cv2.rectangle(img, (10, 60), (60, 110), brush_color, cv2.FILLED)
+    cv2.putText(img, "Color", (10, 105), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
 
     if result.multi_hand_landmarks:
         for handLms in result.multi_hand_landmarks:
@@ -55,14 +67,17 @@ while True:
             thumb_x, thumb_y = int(tm.x * w), int(tm.y * h)
             distance= math.hypot(index_x-thumb_x, index_y-thumb_y)
 
+            if index_y<50:
+                if 0<=index_x<=640:
+                    brush_color=gradient_bar[0,index_x].tolist()
             # Only draw if previous point is not (0,0)
             if prev_x != 0 and prev_y != 0:
                 if fingers[0]==0 and fingers[1]==1 and fingers[2]==1:
                     cv2.circle(img, (index_x, index_y), 10, (255,0,0), cv2.FILLED)
                     cv2.line(canvas, (prev_x, prev_y), (index_x, index_y), (0, 0, 0), 10)
-                elif distance<30:
-                    cv2.circle(img, (index_x, index_y), 10, (255,0,255), cv2.FILLED)
-                    cv2.line(canvas, (prev_x, prev_y), (index_x, index_y), (255, 0, 255), 5)
+                elif distance<25:
+                    cv2.circle(img, (index_x, index_y), 10, brush_color, cv2.FILLED)
+                    cv2.line(canvas, (prev_x, prev_y), (index_x, index_y), brush_color, 5)
 
             # Update previous point
             prev_x, prev_y = index_x, index_y
@@ -72,7 +87,7 @@ while True:
         prev_x, prev_y = 0, 0
 
     # Combine camera frame and drawing canvas
-    img = cv2.addWeighted(img, 0.5, canvas, 0.5, 0)
+    img = cv2.addWeighted(img, 0.7, canvas, 0.7, 0)
 
     cv2.imshow("GestureBoard - Drawing Mode", img)
 
